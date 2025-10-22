c5fcd15f3d30924f9a336e0612c3ba1f00b0e364
Alphin Shajan <alphinshajan2004@gmail.com>
2025-10-22 09:22:12 -0400

    Add automatic test coverage report generation

--- extension/jest.config.ts
+++ extension/jest.config.ts
@@ -39,8 +39,7 @@ const config: Config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: ["json", "text", "lcov", "clover"],
-  coverageReporters: ["text-summary"],
+  coverageReporters: ["text-summary", "html"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
