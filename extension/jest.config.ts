9c653cecb05a384f98869907cd3ca9a6c2248796
alphin-08 <alphinshajan2004@gmail.com>
2025-10-22 21:48:39 -0400

    updated jest config

--- extension/jest.config.ts
+++ extension/jest.config.ts
@@ -39,7 +39,8 @@ const config: Config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ["text-summary", "html"],
+  // Add json-summary to emit coverage/coverage-summary.json consumed by docs generator
+  coverageReporters: ["text-summary", "html", "json-summary"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
